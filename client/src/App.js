c771311df7f1c41866da969f373b973972d05a02
Nathan Langione <nlangione@wayfair.com>
2022-01-23 16:19:28 -0500

    Remove example garbage and add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>sadsa</h1>
       </header>
+      <div className="container">
+        <button type="button" class="btn btn-primary">Primary</button> <br />
+        <button type="button" class="btn btn-secondary">Secondary</button> <br />
+        <button type="butt on" class="btn btn-success">Success</button> <br />
+        <button type="button" class="btn btn-danger">Danger</button> <br />
+        <button type="button" class="btn btn-warning">Warning</button> <br />
+        <button type="button" class="btn btn-info">Info</button> <br />
+        <button type="button" class="btn btn-light">Light</button> <br />
+        <button type="button" class="btn btn-dark">Dark</button> <br />
+        <button type="button" class="btn btn-link">Link</button>
+
+        <br />
+        <div class="card" >
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+          </div>
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
