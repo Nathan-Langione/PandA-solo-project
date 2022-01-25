495dd209280fe8f3ef200a8f933e5714c55e1ecc
Nathan Langione <nlangione@wayfair.com>
2022-01-25 16:59:56 -0500

    removing excess content

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-// No longer needed with bootstrap
-//import './App.css';
 import { Router } from '@reach/router';
 import Landing from './views/Landing';
 import Login from './components/Login';
@@ -8,9 +6,7 @@ import Register from './components/Register';
 import UserProfile from './views/UserProfile';
 import EditUserProfile from './views/EditPetProfile';
 import AllUserProfiles from './views/AllUserProfiles';
-import PetProfile from './views/PetProfile';
-import EditPetProfile from './views/EditPetProfile';
-import AllPetProfiles from './views/AllPetProfiles';
+
 
 function App() {
   return (
@@ -23,9 +19,6 @@ function App() {
         <UserProfile path="/user/:id" />
         <EditUserProfile path="/user/:id/edit" />
         <AllUserProfiles path="/users" />
-        <PetProfile path="/pet/:id" />
-        <EditPetProfile path="/pet/:id/edit" />
-        <AllPetProfiles path="/pets" />
       </Router>
     </div>
   );
