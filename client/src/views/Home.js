8070a736eeae64f1ab463ff7e9b8fe107bbf2a9f
Nathan Langione <nlangione@wayfair.com>
2022-01-30 11:52:10 -0500

    adding links

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -6,58 +6,140 @@ const Home = () => {
 
 
     return (
-        <>
-            <header className="container">
-                <div class="row">
-                    <div class="col-6">
-                        <h1>Boston Common dog park</h1>
-                    </div>
-                    <div class="col">
-                        <Link to={"/login"}>
-                            <h3>Login</h3>
-                        </Link>
-                    </div>
-                    <div class="col">
-                        <Link to={"/register"}>
-                            <h3>Register</h3>
-                        </Link>
-                    </div>
-                </div>
-            </header>
-            <hr />
+        <div class="container-fluid">
 
-            <div className="container">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <h3>Informational links</h3>
-                        <ul>
-                            <li> link</li>
-                            <li> link</li>
-                            <li> link</li>
-                            <li> link</li>
-                            <li> link</li>
-                            <li> link</li>
-                            <li> link</li>
-                            <li> link</li>
-                        </ul>
-
-
-                    </div>
-                    <div class="col">
-                        <div className="img-responsive">
-                            <img src="https://friendsofthepublicgarden.org/wp-content/uploads/2018/06/bostoncommonoverview@kylekleinphoto-1.jpg" alt="new" />
+            <header class="container-fluid">
+                {/* Navbar start */}
+                <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                    <div class="container-fluid">
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-mdb-toggle="collapse"
+                            data-mdb-target="#navbarExample01"
+                            aria-controls="navbarExample01"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <i class="fas fa-bars"></i>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarExample01">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item active">
+                                    <a class="nav-link" aria-current="page" href="/">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/login">Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/register">Register</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About</a>
+                                </li>
+                            </ul>
                         </div>
+                    </div>
+                </nav>
+                {/* Navbar end */}
+
+
+            </header>
+
+            <div class="container-fluid">
+                <div class="row flex-nowrap">
+                    <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                            <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                                <span class="fs-5 d-none d-sm-inline">Useful Links</span>
+                            </a>
+                            <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link align-middle px-0">
+                                        <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                        <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline"> BOSTON COMMON OFF-LEASH DOG PROGRAM</span> </a>
+                                    <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                        <li class="w-100">
+                                            <a href=" https://friendsofthepublicgarden.org/our-parks/the-common/common-canine/" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-link px-0 align-middle">
+                                        <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
+                                </li>
+                                <li>
+                                    <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+                                        <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                                    <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                                        <li class="w-100">
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                        <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
+                                    <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                                        <li class="w-100">
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-link px-0 align-middle">
+                                        <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                                </li>
+                            </ul>
+                            <hr />
+                            <div class="dropdown pb-4">
+                                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle" />
+                                    <span class="d-none d-sm-inline mx-1">loser</span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider" />
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col py-3 text-center">
+                        <h1 class="mb-3">The Boston Common Dog Park</h1>
+                        <h4 class="mb-3">A social networking website</h4>
+                        <img class="mb-3" src="https://friendsofthepublicgarden.org/wp-content/uploads/2018/06/bostoncommonoverview@kylekleinphoto-1.jpg" alt="new" />
                     </div>
                 </div>
             </div>
 
-
-            <footer className="container">
-                <Link to={"/"}>
-                    Go back to the Home page
-                </Link>
+            <footer className="container-fluid text-center">
+                <h1> This is where the footer information goes </h1>
             </footer>
-        </>
+        </div>
     )
 }
 export default Home;
